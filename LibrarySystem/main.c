f0fc95415e90ffd5a419082811667f86bed8172b
haiji <therealjade.1212@gmail.com>
2023-12-13 16:59:06 +0800

    添加id作为内部索引项

--- LibrarySystem/main.c
+++ LibrarySystem/main.c
@@ -14,10 +14,13 @@
  * @brief 账号定义和相关函数
  * 
  */
-#define MAX_ID  7
+#define MAX_NAME    7
 #define MAX_PASSWORD    7
+#define ID_BASE 10020  //用户起始id
+#define ID_ADD  1
 typedef struct Users{
-    char id[MAX_ID];    //用户id
+    int id;    //用户id
+    char name[MAX_NAME];    //用户名字
     char pw[MAX_PASSWORD];  //用户密码
 }Users;
 void ShowAboutLogin();
@@ -54,35 +57,39 @@ int IsLogin(int num){
 void Enroll(){
     Users NewUser, TempUser;
     FILE *fp;   //访问文件的指针
+    int get_time = 0;
 
-    if((fp = fopen("users.txt", "r")) == NULL){ //以只读方式打开users.txt
+    if((fp = fopen("users", "r")) == NULL){ //以只读方式打开users.txt
         printf("无效注册\n");
         return;
     }
 
     printf("\t欢迎来到注册页面!\n");
-    printf("\t请输入 ID, 至多六位\n");
-    scanf("%6s", &NewUser.id);
+    printf("\t请输入用户名, 至多六位\n");
+    scanf("%6s", &NewUser.name);
 
     while (fread(&TempUser, sizeof(Users), 1, fp) == 1) {
-        if (strcmp(NewUser.id, TempUser.id) == 0) {
-            printf("\t此id已存在,请重新注册!\n");
+        if (strcmp(NewUser.name, TempUser.name) == 0) {
+            printf("\t此用户名已存在,请重新注册!\n");
             fclose(fp);
             return;
         }
+        get_time ++;
     }
     fclose(fp);
 
-    printf("\t请输入 密码, 至多六位\n");
+    printf("\t请输入密码, 至多六位\n");
 	scanf("%6s", &NewUser.pw);
 
-    if((fp = fopen("users.txt", "a")) == NULL){ //以写方式打开users.txt， w会覆盖原文件内容，这里用a
+    NewUser.id = ID_BASE + ID_ADD * get_time;
+
+    if((fp = fopen("users", "a")) == NULL){ //以写方式打开users.txt， w会覆盖原文件内容，这里用a
         printf("\t账号注册失败,请重试\n");
         return;
     }
-
     fwrite(&NewUser, sizeof(Users), 1, fp);
 	printf("\t账号注册成功!\n");
+    printf("\t您的id是: %d\n", NewUser.id);
 	fclose(fp);
 	fp = NULL;
     return;
@@ -96,21 +103,23 @@ void Login(){
     Users OldUser, TempUser;
     FILE *fp;   //访问文件的指针
 
-    if((fp = fopen("users.txt", "r")) == NULL){ //以只读方式打开users.txt
+    if((fp = fopen("users", "r")) == NULL){ //以只读方式打开users.txt
         printf("无效登录\n");
         return;
     }   
 
     printf("\t欢迎来到登录页面!\n");
-    printf("\t请输入 ID, 至多六位\n");
-    scanf("%6s", &OldUser.id);
+    printf("\t请输入用户名\n");
+    scanf("%6s", &OldUser.name);
 
     while (fread(&TempUser, sizeof(Users), 1, fp) == 1) {
-        if (strcmp(OldUser.id, TempUser.id) == 0) {
-            printf("\t请输入 密码, 至多六位\n");
+        if (strcmp(OldUser.name, TempUser.name) == 0) {
+            printf("\t请输入密码, 至多六位\n");
 	        scanf("%6s", &OldUser.pw);
             if(strcmp(OldUser.pw, TempUser.pw) == 0){
                 printf("\t账号登录成功!\n");  
+                printf("\t欢迎%s!\n", TempUser.name);  
+                printf("\t您的id为%d!\n", TempUser.id);  
                 fclose(fp);
                 //todo:登录之后的菜单显示  
                 return;
