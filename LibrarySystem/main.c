4bff09d03a644e4718201bd595c618dcf59f9c8e
haiji <therealjade.1212@gmail.com>
2023-12-15 21:22:35 +0800

    完成浏览图书信息&添加图书&浏览用户信息的功能

--- LibrarySystem/main.c
+++ LibrarySystem/main.c
@@ -10,6 +10,7 @@
  */
 #include<stdio.h>
 #include<string.h>
+
 /**
  * @brief 账号定义和相关函数
  * 
@@ -18,6 +19,7 @@
 #define MAX_PASSWORD    7
 #define ID_BASE 10020  //用户起始id
 #define ID_ADD  1
+#define ADMIN_NAME  "admin"   //管理员名称
 typedef struct Users{
     int id;    //用户id
     char name[MAX_NAME];    //用户名字
@@ -28,6 +30,36 @@ int IsLogin(int num);
 void Enroll();
 void Login();
 void ChangePW();
+void ShowAtAdmin();
+void ShowAtUser();
+void ScanBook();
+void AddBook();
+void ScanUser();
+
+/**
+ * @brief 图书定义和相关函数
+ * 
+ */
+#define MAX_LIB_NAME    20
+#define MAX_LIB_AUTHOR  20
+#define MAX_LIB_PUBLISH 20
+#define ID_LIB_BASE 520  //书籍起始索引号
+#define ID_LIB_ADD  1
+enum LibCategory{
+    Science = 1,
+    Business,   //2
+    Poetry,     //3
+    Cookbook,   //4
+    Fiction,    //5
+};
+typedef struct Libs{
+    int index;  //书籍索引号
+    char libname[MAX_LIB_NAME];    //书籍名称
+    char authorname[MAX_LIB_AUTHOR];    //作者名称
+    char publishname[MAX_LIB_PUBLISH];  //出版商名称
+    enum LibCategory category;  //书籍类别
+    int num;    //书籍进库量
+}Libs;
 
 /**
  * @brief 注册登录页面显示
@@ -122,7 +154,10 @@ void Login(){
                 printf("\t欢迎%s!\n", TempUser.name);  
                 printf("\t您的id为%d!\n", TempUser.id);  
                 fclose(fp);
-                //todo:登录之后的菜单显示  
+                //todo:登录之后的菜单显示
+                if(strcmp(TempUser.name, ADMIN_NAME) == 0){   //如果是管理员，进入管理员页面
+                    ShowAtAdmin();
+                }  
                 return;
             }
             else{
@@ -175,6 +210,163 @@ void ChangePW(){
             break;
         }
     }
+    return;
+}
+
+/**
+ * @brief 管理者页面显示
+ * 
+ */
+void ShowAtAdmin(){
+    printf("*************************************\n");
+    printf("************* 您好 Admin ************\n");
+    printf("******** 浏览图书库存,请输入 1 ********\n");
+    printf("********** 删除图书,请输入 2 *********\n");
+    printf("********** 添加图书,请输入 3 *********\n");
+    printf("******** 浏览用户信息,请输入 4 ********\n");
+    printf("*************************************\n");
+    int get_num;
+    scanf("%d", &get_num);
+    if(get_num == 1){   //浏览图书库存
+        ScanBook();
+    }else if(get_num == 2){ //删除图书
+
+    }else if(get_num == 3){ //添加图书
+        AddBook();
+    }else if(get_num == 4){     //浏览用户信息
+        ScanUser();
+    }else{
+        printf("操作无效!\n");
+        return;
+    }
+}
+
+/**
+ * @brief 浏览图书信息
+ * 
+ */
+void ScanBook(){
+    Libs TempBook;
+    FILE *fp;   //访问文件的指针
+
+    if((fp = fopen("libs", "r")) == NULL){ //以只读方式打开libs
+        printf("操作失败,请重试!\n");
+        return;
+    }   
+    printf("*****LibSys 当前书籍*****\n");
+    printf("ID\t书名\t\t\t\t作者\t\t\t出版社\t\t\t\t类别\t\t\t库存量\n");
+    while (fread(&TempBook, sizeof(Libs), 1, fp) == 1) {
+        char temp_category[10];
+        switch(TempBook.category){
+            case Science:
+                strcpy(temp_category, "Science");
+                break;
+            case Business:
+                strcpy(temp_category, "Business");
+                break;
+            case Poetry:
+                strcpy(temp_category, "Poetry");
+                break;
+            case Cookbook:
+                strcpy(temp_category, "Cookbook");
+                break;
+            case Fiction:
+                strcpy(temp_category, "Fiction");
+                break;
+            default:
+                break;
+        }
+        printf("%d\t%s\t\t\t%s\t\t\t%s\t\t\t%s\t\t\t%d\n", TempBook.index, TempBook.libname, TempBook.authorname, TempBook.publishname, temp_category, TempBook.num);
+    }
+    fclose(fp);
+    return;
+}
+
+/**
+ * @brief 添加图书
+ * 
+ */
+void AddBook(){
+    Libs NewBook, TempBook;
+    FILE *fp;   //访问文件的指针
+    int get_time = 0;
+
+    if((fp = fopen("libs", "r")) == NULL){ //以只读方式打开libs
+        printf("无效操作,请重试!\n");
+        return;
+    }
+
+    printf("请输入新入库图书信息\n");
+    printf("新入库图书:");
+    scanf("%20s", NewBook.libname);
+
+    while (fread(&TempBook, sizeof(Libs), 1, fp) == 1) {
+        if (strcmp(NewBook.libname, TempBook.libname) == 0) {
+            printf("书库已有此书\n");
+            //todo:若书库有这本书，则更新书籍数量信息
+            fclose(fp);
+            return;
+        }
+        get_time ++;
+    }
+    fclose(fp);
+
+    printf("作者:");
+	scanf("%20s", NewBook.authorname);
+    printf("出版社:");
+	scanf("%20s", NewBook.publishname);
+    printf("Science:1; Business:2; Poetry:3; Cookbook:4; Fiction:5\n");
+    printf("图书类别:");
+	scanf("%d", &NewBook.category);
+    printf("入库数量:");
+	scanf("%d", &NewBook.num);
+
+    NewBook.index = ID_LIB_BASE + ID_LIB_ADD * get_time;
+
+    if((fp = fopen("libs", "a")) == NULL){ //以写方式打开libs， w会覆盖原文件内容，这里用a
+        printf("\t添加失败,请重试\n");
+        return;
+    }
+    fwrite(&NewBook, sizeof(Libs), 1, fp);
+	printf("\t添加成功!\n");
+	fclose(fp);
+	fp = NULL;
+    return;    
+}
+
+/**
+ * @brief 浏览用户信息
+ * 
+ */
+void ScanUser(){
+    Users TempUser;
+    FILE *fp;   //访问文件的指针
+
+    if((fp = fopen("users", "r")) == NULL){ //以只读方式打开users
+        printf("操作失败,请重试!\n");
+        return;
+    }   
+    printf("*****LibSys 用户名单*****\n");
+    printf("ID\t用户名\t密码\n");
+    while (fread(&TempUser, sizeof(Users), 1, fp) == 1) {
+        printf("%d\t%s\t%s\n", TempUser.id, TempUser.name, TempUser.pw);
+    }
+    fclose(fp);
+    return;
+}
+
+/**
+ * @brief 普通用户页面显示
+ * 
+ */
+void ShowAtUser(){
+    printf("*******************************\n");
+    printf("******* 您好 xx ********\n");   //TODO:需要传入实际用户信息
+    printf("******* 浏览图书库存,请输入 1 ********\n");
+    printf("******* 图书查询,请输入 2 ********\n");
+    printf("******* 借阅图书,请输入 3 ********\n");
+    printf("******* 修改密码,请输入 4 ********\n");
+    printf("*******************************\n");
 }
 
 /**
